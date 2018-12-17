aba9d1297133a2def6972adbb9af9c5053673487
DrRainMan87 <alessiojedy@gmail.com>
2018-12-17 23:46:59 +0100

    Creata classe TestFumetto...

---
+++ ComicsInventory/src/test/TestFumetto.java
@@ -0,0 +1,29 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestFumetto {
+
+	@Test
+	void testInserisciFumetto() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testModificaFumetto() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCancellaFumetto() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCercaFumetto() {
+		fail("Not yet implemented");
+	}
+
+}
